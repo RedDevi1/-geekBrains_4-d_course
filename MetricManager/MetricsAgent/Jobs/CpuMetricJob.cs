604c35580e6b6a4862c2c271ddf3bcbf86df134e
Nikita <nickita.sevsk@gmail.com>
2022-03-28 14:44:02 +0300

    Lesson_5_Started

---
+++ MetricManager/MetricsAgent/Jobs/CpuMetricJob.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MetricsAgent.DAL.Interfaces;
+using Quartz;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Jobs
+{
+    public class CpuMetricJob : IJob
+    {
+        private ICpuMetricsRepository _repository;
+        public CpuMetricJob(ICpuMetricsRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            // Теперь можно записать что-то посредством репозитория
+            return Task.CompletedTask;
+        }
+    }
+}
